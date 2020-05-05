48faba93bfdc4830b6575950378134ca57ed4646
adsd <michaelnurilov67@gmail.com>
2020-05-05 17:47:06 -0400

    Initial Commit

---
+++ Gamer Guider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gamer Guider
+//
+//  Created by Michael Nurilov on 5/5/20.
+//  Copyright © 2020 Michael Nurilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
