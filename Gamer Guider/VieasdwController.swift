985fa5c46e720f745b2e8df2d481b065d35df8ae
adsd <michaelnurilov67@gmail.com>
2020-05-11 04:31:14 -0400

    The core project is completed, just gotta add all those extra requirements and then make it look pretty

---
+++ Gamer Guider/VieasdwController.swift
@@ -0,0 +1,30 @@
+//
+//  VieasdwController.swift
+//  Gamer Guider
+//
+//  Created by Michael Nurilov on 5/11/20.
+//  Copyright © 2020 Michael Nurilov. All rights reserved.
+//
+
+import UIKit
+
+class VieasdwController: UISearchController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Gamer Guider/ViewController.swift
+++ Gamer Guider/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var myTableView: UITableView!
     var games = [Game]()
+    var filtered = [Game]()
     
     @IBOutlet weak var mySearchBar: UISearchBar!
     
@@ -24,8 +25,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         myTableView.dataSource = self
         myTableView.delegate = self
         
+        mySearchBar.showsScopeBar = true
+        mySearchBar.scopeButtonTitles = ["All", "PC", "PS4", "Xbox One", "Switch"]
         mySearchBar.delegate = self
-        mySearchBar.scopeButtonTitles = ["Genre, Platform"]
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -35,7 +37,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return games.count
+        return filtered.count
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -51,55 +53,55 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
             if let indexPath = self.myTableView.indexPathForSelectedRow {
                 let controller = segue.destination as! GameViewController
-                if let variableName = games[indexPath.row].name {
+                if let variableName = filtered[indexPath.row].name {
                     controller.name = variableName
                 }
                 else {
                     controller.name = ""
                 }
-                if let variableName = games[indexPath.row].id {
+                if let variableName = filtered[indexPath.row].id {
                     controller.id = variableName
                 }
                 else {
                     controller.id = 0
                 }
-                if let variableName = games[indexPath.row].first_release_date {
+                if let variableName = filtered[indexPath.row].first_release_date {
                     controller.first_release_date = variableName
                 }
                 else {
                     controller.first_release_date = 0
                 }
-                if let variableName = games[indexPath.row].cover {
+                if let variableName = filtered[indexPath.row].cover {
                     controller.cover = variableName
                 }
                 else {
                     controller.cover = 0
                 }
-                if let variableName = games[indexPath.row].popularity {
+                if let variableName = filtered[indexPath.row].popularity {
                     controller.popularity = variableName
                 }
                 else {
                     controller.popularity = 0
                 }
-                if let variableName = games[indexPath.row].total_rating {
+                if let variableName = filtered[indexPath.row].total_rating {
                     controller.total_rating = variableName
                 }
                 else {
                     controller.total_rating = 0
                 }
-                if let variableName = games[indexPath.row].summary {
+                if let variableName = filtered[indexPath.row].summary {
                     controller.summary = variableName
                 }
                 else {
                     controller.summary = ""
                 }
-                if let variableName = games[indexPath.row].genres {
+                if let variableName = filtered[indexPath.row].genres {
                     controller.genres = variableName
                 }
                 else {
                     controller.genres = []
                 }
-                if let variableName = games[indexPath.row].platforms {
+                if let variableName = filtered[indexPath.row].platforms {
                     controller.platforms = variableName
                 }
                 else {
@@ -113,7 +115,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "GameItem", for: indexPath)
         let nameLabel = cell.viewWithTag(100) as! UILabel
           
-        let game = games[indexPath.row]
+        let game = filtered[indexPath.row]
         nameLabel.text = game.name
         
         // End of new code block
@@ -132,11 +134,79 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         print(searchText)
     }
     
+    /*func filtertableView(platform:Int){
+        switch platform {
+        case 0:
+            let filtered = games.filter { $0.platforms?.contains(6) == true }
+
+            
+        default:
+        }
+    }*/
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        print("New scope index is now \(selectedScope)")
+        
+        switch selectedScope {
+        case 0:
+            // ALL
+            filtered = games
+            myTableView.reloadData()
+        case 1:
+            // PC
+            filtered = games.filter { $0.platforms?.contains(6) == true }
+            myTableView.reloadData()
+        case 2:
+            // PS4
+            filtered = games.filter { $0.platforms?.contains(48) == true }
+            myTableView.reloadData()
+        case 3:
+            // XBOX ONE
+            filtered = games.filter { $0.platforms?.contains(49) == true }
+            myTableView.reloadData()
+        case 4:
+            // SWITCH
+            filtered = games.filter { $0.platforms?.contains(130) == true }
+            myTableView.reloadData()
+        default:
+            print("Howd you get here?")
+        }
+    }
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("SEARCH BUTTON MOTHER FUCKIGN CLICKED")
         print(mySearchBar.text!)
         
         let session = URLSession.shared
+        let url = URL(string: "https://api-v3.igdb.com/games/")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        request.setValue("37fb4484f0596a97d1fd1b576f2e1c80", forHTTPHeaderField: "user-key")
+        request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
+
+        let body = "fields name,first_release_date,genres,platforms,popularity,total_rating,summary,cover; search \"\(mySearchBar.text!)\"; limit 100;".data(using: .utf8)!
+
+        let task = session.uploadTask(with: request, from: body) { data, response, error in
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                let json_encoded = dataString.data(using: .utf8)!
+                
+                do {
+                    let res = try JSONDecoder().decode([Game].self, from: json_encoded)
+                    self.games = res
+                    self.filtered = self.games
+                    self.myTableView.reloadData()
+                } catch {
+                    print(error.localizedDescription)
+                }
+                
+            }
+        }
+
+        task.resume()
+        
+        /*let session = URLSession.shared
         let url = URL(string: "https://api-v3.igdb.com/search")!
         
         var request = URLRequest(url: url)
@@ -204,7 +274,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
         }
 
-        task.resume()
+        task.resume()*/
     }
     
     @IBAction func api_call() {
@@ -218,7 +288,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         request.setValue("37fb4484f0596a97d1fd1b576f2e1c80", forHTTPHeaderField: "user-key")
         request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
 
-        let body = "fields name,first_release_date,genres,platforms,popularity,total_rating,summary,cover; sort popularity desc; limit 100;".data(using: .utf8)!
+        let body = "fields name,first_release_date,genres,platforms,popularity,total_rating,summary,cover; sort popularity desc; limit 10;".data(using: .utf8)!
 
         let task = session.uploadTask(with: request, from: body) { data, response, error in
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
@@ -227,6 +297,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 do {
                     let res = try JSONDecoder().decode([Game].self, from: json_encoded)
                     self.games = res
+                    self.filtered = self.games
                     self.myTableView.reloadData()
                 } catch {
                     print(error.localizedDescription)
